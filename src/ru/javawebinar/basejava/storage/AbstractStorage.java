9bd23077fbb19d1f25876d553484d4db62c65418
evgeny <ssbscrib@gmail.com>
2025-01-21 20:58:26 +0500

    HW_5 edit AbstractStorage

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -9,10 +9,19 @@ public abstract class AbstractStorage implements Storage {
     }
 
     public Resume[] getAll() {
+
         return new Resume[0];
     }
 
     public int size() {
+
         return 0;
     }
+    public abstract void update(Resume r);
+
+    public abstract Resume get(String uuid);
+
+    public abstract void delete(String uuid);
+
+    public abstract void save(Resume r);
 }
