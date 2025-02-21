d44821761460f3014d9ddb893e00db8c672afb6d
evgeny <ssbscrib@gmail.com>
2025-02-21 20:30:34 +0500

    HW_5 add ListStorage

--- src/ru/javawebinar/basejava/storage/ListStorage.java
+++ src/ru/javawebinar/basejava/storage/ListStorage.java
@@ -2,49 +2,60 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListStorage extends AbstractStorage {
+
+    private final List<Resume> list = new ArrayList<>();
+
     @Override
     protected Object getSearchKey(String uuid) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getUuid().equals(uuid)) {
+                return i;
+            }
+        }
         return null;
     }
 
     @Override
     protected void doUpdate(Resume r, Object searchKey) {
-
+        list.set((Integer) searchKey, r);
     }
 
     @Override
     protected boolean isExist(Object searchKey) {
-        return false;
+        return searchKey != null;
     }
 
     @Override
     protected void doSave(Resume r, Object searchKey) {
-
+        list.add(r);
     }
 
     @Override
     protected Resume doGet(Object searchKey) {
-        return null;
+        return list.get((Integer) searchKey);
     }
 
     @Override
     protected void doDelete(Object searchKey) {
-
+        list.remove(((Integer) searchKey).intValue());
     }
 
     @Override
     public void clear() {
-
+        list.clear();
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return list.toArray(new Resume[list.size()]);
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 }
